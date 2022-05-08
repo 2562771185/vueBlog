6c444319ea975a2f8451673ff50b41b3045bf894
JHZZ_mjz <m2562771185@163.com>
2022-05-08 13:14:44 +0800

    基本架构

---
+++ src/main/java/com/jhzz/vueblogservice/VueblogServiceApplication.java
@@ -0,0 +1,13 @@
+package com.jhzz.vueblogservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VueblogServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VueblogServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jhzz/vueblogservice/VueblogServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jhzz.vueblogservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VueblogServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
